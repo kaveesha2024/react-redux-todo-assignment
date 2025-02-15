384faec38ce6543229f764096d6b47a489a0c3bd
Kaveesha Sashen <sanaakaveesha@gmail.com>
2025-02-15 15:44:11 +0530

    Installed MUI, And created UI for task adding

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
-const  App = () => {
+import { Button, TextField } from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
+import {useState} from "react";
+
+const App = () => {
+    const [task, setTask] = useState('')
+    const taskHandler = (event) => {
+        setTask(event.target.value)
+    };
 
   return (
     <>
-
+      <div className="m-4">
+        <TextField
+          className="m-4"
+          name="task"
+          id="standard-basic"
+          label="Standard"
+          variant="standard"
+          onChange={taskHandler}
+        />
+        <Button variant="contained" endIcon={<SendIcon />}>
+          Add
+        </Button>
+          <div className="mt-4">
+              <p>
+                  {task}
+              </p>
+          </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
